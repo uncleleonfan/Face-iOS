1eaa7e178140682a07ae9b2e0e9df43581578725
leon <526247082@qq.com>
2018-11-05 16:46:40 +0800

    Initial Commit

---
+++ Faceit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by Leon Fan on 2018/11/5.
+//  Copyright © 2018 Leon Fan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
